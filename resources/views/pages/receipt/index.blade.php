c096eebd389d177d033dce73e26d13196ed95968
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2024-01-10 19:54:39 +0300

    !fix

--- resources/views/pages/receipt/index.blade.php
+++ resources/views/pages/receipt/index.blade.php
@@ -187,14 +187,18 @@
                     </div>
                 </form>
                 <div class="receipt-get__content">
-                    <div class="receipt-list">
-                        @foreach ($receipts as $item)
-                            <x-receipt-item :receipt="$item" />
-                        @endforeach
-                    </div>
-                    <div class="pagination-margin">
-                        {{ $receipts->appends(Request::all())->links('vendor.pagination') }}
-                    </div>
+                    @if ($receipts->count())
+                        <div class="receipt-list">
+                            @foreach ($receipts as $item)
+                                <x-receipt-item :receipt="$item" />
+                            @endforeach
+                        </div>
+                        <div class="pagination-margin">
+                            {{ $receipts->appends(Request::all())->links('vendor.pagination') }}
+                        </div>
+                    @else
+                        <div class="not-found">Ничего не найдено</div>
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/pages/receipt/trash.blade.php
+++ resources/views/pages/receipt/trash.blade.php
@@ -5,27 +5,31 @@
         <div class="container">
             <div class="receipt-get__container">
                 <div class="receipt-get__content">
-                    <div class="receipt-list">
-                        @foreach ($receipts as $item)
-                            <x-receipt-item :receipt="$item">
-                                <x-slot:action>
-                                    <form action="{{ route('receipt.restore', ['id' => $item->id]) }}" method="POST">
-                                        @csrf
-                                        {{ method_field('PATCH') }}
-                                        <button class="link-green">Восстановить</button>
-                                    </form>
-                                    <form action="{{ route('receipt.forceDelete', ['id' => $item->id]) }}" method="POST">
-                                        @csrf
-                                        {{ method_field('DELETE') }}
-                                        <button class="link-red">Удалить</button>
-                                    </form>
-                                </x-slot:action>
-                            </x-receipt-item>
-                        @endforeach
-                    </div>
-                    <div class="pagination-margin">
-                        {{ $receipts->appends(Request::all())->links('vendor.pagination') }}
-                    </div>
+                    @if ($receipts->count())
+                        <div class="receipt-list">
+                            @foreach ($receipts as $item)
+                                <x-receipt-item :receipt="$item">
+                                    <x-slot:action>
+                                        <form action="{{ route('receipt.restore', ['id' => $item->id]) }}" method="POST">
+                                            @csrf
+                                            {{ method_field('PATCH') }}
+                                            <button class="link-green">Восстановить</button>
+                                        </form>
+                                        <form action="{{ route('receipt.forceDelete', ['id' => $item->id]) }}" method="POST">
+                                            @csrf
+                                            {{ method_field('DELETE') }}
+                                            <button class="link-red">Удалить</button>
+                                        </form>
+                                    </x-slot:action>
+                                </x-receipt-item>
+                            @endforeach
+                        </div>
+                        <div class="pagination-margin">
+                            {{ $receipts->appends(Request::all())->links('vendor.pagination') }}
+                        </div>
+                    @else
+                        <div class="not-found">Корзина пуста</div>
+                    @endif
                 </div>
             </div>
         </div>
