7152410ed9f7ae3787ee87aad9f4b1244884d3a7
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2024-01-14 12:38:19 +0300

    !

--- app/Http/Requests/StoreReceiptUploaderRequest.php
+++ app/Http/Requests/StoreReceiptUploaderRequest.php
@@ -12,6 +12,7 @@ class StoreReceiptUploaderRequest extends FormRequest
      */
     public function authorize(): bool
     {
+        dd($this->chat_id);
         return auth()->check() || $this->chat_id;
     }
 
