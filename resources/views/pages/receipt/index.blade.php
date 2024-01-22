f292cff7946f0b9a9ad3439cee58dd9b898b83c1
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2024-01-22 14:29:48 +0300

    !fix

--- resources/views/pages/receipt/index.blade.php
+++ resources/views/pages/receipt/index.blade.php
@@ -143,10 +143,10 @@
                                     <input class="checbox__input" type="checkbox">
                                     <span class="checbox__icon"></span>
                                     <span class="label__title">Только с НДС</span>
-                                    <input type="hidden" name="filterGEQ[nds0]" value="1">
+                                    {{-- <input type="hidden" name="filterGEQ[nds0]" value="1">
                                     <input type="hidden" name="filterGEQ[ndsNo]" value="1">
                                     <input type="hidden" name="filterGEQ[nds10]" value="1">
-                                    <input type="hidden" name="filterGEQ[nds18]" value="1">
+                                    <input type="hidden" name="filterGEQ[nds18]" value="1"> --}}
                                 </label>
                             </div>
                         </details>
