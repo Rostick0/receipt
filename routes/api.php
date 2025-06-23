4dfc3767049b8844f44f7b532f0363f296c123d4
Rostislav <rostik057@gmail.com>
2025-06-23 23:41:40 +0300

    !fix route

--- routes/api.php
+++ routes/api.php
@@ -28,56 +28,53 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::group([
-    'middleware' => [
-        'api',
-    ],
+Route::middleware('api')
+    ->name('api.')
+    ->group(function () {
+        Route::group([
+            'prefix' => 'auth'
+        ], function () {
+            Route::post('login', [AuthController::class, 'login']);
 
-], function () {
-    Route::group([
-        'prefix' => 'auth'
-    ], function () {
-        Route::post('login', [AuthController::class, 'login']);
+            Route::group([
+                'middleware' => 'auth:api'
+            ], function () {
+                Route::get('me', [AuthController::class, 'me']);
+                Route::post('logout', [AuthController::class, 'logout']);
+            });
+        });
 
         Route::group([
             'middleware' => 'auth:api'
         ], function () {
-            Route::get('me', [AuthController::class, 'me']);
-            Route::post('logout', [AuthController::class, 'logout']);
+            Route::post('receipt-upload', [ReceiptUploaderController::class, 'store'])->name('receipt-upload.site.store');
+
+            Route::apiResource('folder', FolderController::class)->only(['index']);
+            Route::apiResource('folder-receipt', FolderReceiptController::class)->only(['store', 'destroy']);
+        });
+
+        Route::apiResource('receipt', ReceiptController::class)->only(['index']);
+        Route::apiResource('receipt-upload', ReceiptUploaderController::class)->only(['index', 'show']);
+
+        Route::get('okved', [OkvedController::class, 'index']);
+
+        Route::group([
+            'prefix' => 'telegram',
+            'middleware' => 'telegram.token'
+        ], function () {
+            Route::get('user/me/{telegram_user_id}', [UserTelegramController::class, 'me']);
+            Route::apiResource('user', UserTelegramController::class)->only(['store', 'destroy']);
+
+            Route::apiResource('receipt-upload', TelegramReceiptUploaderController::class)->only(['store']);
+            Route::apiResource('folder', TelegramFolderController::class)->only(['store']);
+
+            // Route::post('receipt-upload', [ReceiptUploaderController::class, 'store']);
+        });
+
+        Route::group([
+            'prefix' => 'integration',
+            'middleware' => 'integration.token'
+        ], function () {
+            Route::post('receipt-upload', [IntegrationReceiptUploaderController::class, 'store']);
         });
     });
-
-    Route::group([
-        'middleware' => 'auth:api'
-    ], function () {
-        Route::post('receipt-upload', [ReceiptUploaderController::class, 'store'])->name('receipt-upload.site.store');
-
-        Route::apiResource('folder', FolderController::class)->only(['index']);
-        Route::apiResource('folder-receipt', FolderReceiptController::class)->only(['store', 'destroy']);
-    });
-
-    Route::apiResource('receipt', ReceiptController::class)->only(['index']);
-    Route::apiResource('receipt-upload', ReceiptUploaderController::class)->only(['index', 'show']);
-
-    Route::get('okved', [OkvedController::class, 'index']);
-
-    Route::group([
-        'prefix' => 'telegram',
-        'middleware' => 'telegram.token'
-    ], function () {
-        Route::get('user/me/{telegram_user_id}', [UserTelegramController::class, 'me']);
-        Route::apiResource('user', UserTelegramController::class)->only(['store', 'destroy']);
-
-        Route::apiResource('receipt-upload', TelegramReceiptUploaderController::class)->only(['store']);
-        Route::apiResource('folder', TelegramFolderController::class)->only(['store']);
-
-        // Route::post('receipt-upload', [ReceiptUploaderController::class, 'store']);
-    });
-
-    Route::group([
-        'prefix' => 'integration',
-        'middleware' => 'integration.token'
-    ], function () {
-        Route::apiResource('receipt-upload', IntegrationReceiptUploaderController::class)->only(['store']);
-    });
-});
